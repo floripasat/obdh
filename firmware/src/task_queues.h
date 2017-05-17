43ff07fe84ed1ab906e9479233ef6f628f1d1eb2
Elder Tramontin <elderdtramontin@gmail.com>
2017-05-17 17:25:55 -0300

    Create queues to verify new data from tasks, in store task (#14, #89)

--- firmware/src/task_queues.h
+++ firmware/src/task_queues.h
@@ -12,16 +12,25 @@
 
 
 /** Queues wait time in ticks. The value should be write in miliseconds and will be converted to ticks. */
+#define     SYSTEM_STATUS_QUEUE_WAIT_TIME           (100 / portTICK_PERIOD_MS)
 #define     IMU_QUEUE_WAIT_TIME                     (100 / portTICK_PERIOD_MS)
 #define     INTERNAL_SENSORS_QUEUE_WAIT_TIME        (100 / portTICK_PERIOD_MS)
+#define     SOLAR_PANELS_QUEUE_WAIT_TIME            (100 / portTICK_PERIOD_MS)
+#define     TRANSCEIVER_QUEUE_WAIT_TIME             (100 / portTICK_PERIOD_MS)
 #define     EPS_QUEUE_WAIT_TIME                     (100 / portTICK_PERIOD_MS)
-#define     SYSTEM_STATUS_QUEUE_WAIT_TIME          (100 / portTICK_PERIOD_MS)
+#define     PAYLOAD1_QUEUE_WAIT_TIME                (100 / portTICK_PERIOD_MS)
+#define     PAYLOAD2_QUEUE_WAIT_TIME                (100 / portTICK_PERIOD_MS)
 
 
+volatile xQueueHandle system_status_queue;
 volatile xQueueHandle imu_queue;
 volatile xQueueHandle internal_sensors_queue;
+volatile xQueueHandle solar_panels_queue;
+volatile xQueueHandle transceiver_queue;
 volatile xQueueHandle eps_queue;
-volatile xQueueHandle system_status_queue;
+volatile xQueueHandle payload1_queue;
+volatile xQueueHandle payload2_queue;
+
 
 volatile xQueueHandle status_eps_queue, status_payload1_queue, status_payload2_queue, status_imu_queue, status_mem1_queue;
 //TODO: mem2, antenna status_queue
