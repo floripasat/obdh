c68773bb62977403322e27244a7bfb0baecc9553
Elder Tramontin <elderdtramontin@gmail.com>
2017-07-03 18:16:48 -0300

    When in Shutdown mode, turn-off all readings (#24).

--- firmware/include/floripasat_def.h
+++ firmware/include/floripasat_def.h
@@ -73,6 +73,22 @@ typedef struct {
     uint8_t payload2           [100]; 
 } data_packet_t;
 
+typedef struct {
+    uint8_t batteries[12];          /**< bat_1_voltage[2], bat_2_voltage[2], bat_1_temp[3], bat_2_temp[3], bat_charge[2] */
+    uint8_t solar_panels[18];       /**< sp_1_volt[2], sp_2_volt[2], sp_3_volt[2], sp_1_cur[2], sp_2_cur[2], sp_3_cur[2], sp_4_cur[2], sp_5_cur[2], sp_6_cur[2] */
+    uint8_t imu[12];                /**< accel_0_x[1], accel_0_y[1], accel_0_z[1], gyr_0_x[1], gyr_0_y[1], gyr_0_z[1], accel_1_x[1], accel_1_y[1], accel_1_z[1], gyr_1_x[1], gyr_1_y[1], gyr_1_z[1] */
+    uint8_t system_time[4];         /**< time, in minutes, since deploy */
+    uint8_t satellite_status[2];    /**< energy_mode,  */
+    uint8_t reset_counter[3];       /**< increment each time a reset happens */
+} beacon_packet_t;
+
+typedef struct {
+    uint8_t start_of_byte;          /**< 0x7E */
+    beacon_packet_t data;           /**< valid data */
+    uint8_t crc_result;             /**< 8-bit crc value of the data */
+} ttc_packet_t;
+
+
 /**
  * \defgroup current_state_masks
  * \{
--- firmware/src/FreeRTOSConfig.h
+++ firmware/src/FreeRTOSConfig.h
@@ -139,6 +139,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay				1
+#define INCLUDE_xTaskGetHandle          1
 
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
 
