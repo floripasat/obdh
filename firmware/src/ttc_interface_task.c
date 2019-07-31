aa54d809cd1af5e2a824a1e9ff96cf034924a8bf
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-07-31 16:04:57 -0300

    Adding debug messages to the TTC and RUSH interfaces tasks

--- firmware/src/ttc_interface_task.c
+++ firmware/src/ttc_interface_task.c
@@ -20,31 +20,38 @@
  *
  */
 
- /**
- * \file ttc_interface_task.c
- *
+/**
  * \brief Task that deal with the TTC interface
  *
  * \author Elder Tramontin
  *
+ * \version 0.2.12
+ *
+ * \addtogroup ttc_interface_task
+ * \{
  */
+
+#include "../interface/debug/debug.h"
 
 #include "ttc_interface_task.h"
 
-#define MILISECONDS_IN_A_DAY    86400000      //1000ms * 60s * 60min * 24hr
+#define MILISECONDS_IN_A_DAY    86400000      // 1000ms * 60s * 60min * 24hr
 #define TIME_TO_PROCESS_CMD     2000          /**< 2 seconds */
 
 uint8_t send_mutex_request();
 void send_shutdown();
 
-void ttc_interface_task( void *pvParameters ) {
+void ttc_interface_task(void *pvParameters) {
+    debug_print_event_from_module(DEBUG_INFO, "Tasks", "Initializing TTC interface task...");
+    debug_new_line();
+
     TickType_t last_wake_time;
     last_wake_time = xTaskGetTickCount();
     uint8_t cmd_to_send = 0;
     uint8_t tx_allow = 1;
     TaskHandle_t store_task_handle;
 
-    /**< get the handle of store_data_task, to stop the sampling, when in shutdown mode */
+    // get the handle of store_data_task, to stop the sampling, when in shutdown mode
     store_task_handle = xTaskGetHandle("StoreData");
 
     while(1) {
@@ -53,37 +60,36 @@ void ttc_interface_task( void *pvParameters ) {
         send_data_packet();
         xSemaphoreGive(fsp_semaphore);
 
-        if(xQueueReceive(ttc_queue, (void *) &cmd_to_send, 100) == pdPASS) {
-
+        if (xQueueReceive(ttc_queue, (void *) &cmd_to_send, 100) == pdPASS) {
             if (cmd_to_send == TTC_CMD_HIBERNATION) {           // if it is a shutdown cmd
                 send_shutdown();
 
-                vTaskSuspend(store_task_handle);                /**< stop the store task and, by consequence, the tasks that do readings */
+                vTaskSuspend(store_task_handle);                // stop the store task and, by consequence, the tasks that do readings
 #ifdef _DEBUG
-                vTaskDelayMs(200000);                            /**< this delay is a shutdown simulation */
+                vTaskDelayMs(200000);                           // this delay is a shutdown simulation
 #else
-                vTaskDelayMs(MILISECONDS_IN_A_DAY);             /**< wait 24 hours */
+                vTaskDelayMs(MILISECONDS_IN_A_DAY);             // wait 24 hours
 #endif
                 vTaskResume(store_task_handle);
             }
 
-            if(cmd_to_send == TTC_CMD_TX_MUTEX_REQUEST) {       /**< if it is a mutex request cmd */
-                if(send_mutex_request() == TTC_ACK) {
+            if (cmd_to_send == TTC_CMD_TX_MUTEX_REQUEST) {      // if it is a mutex request cmd
+                if (send_mutex_request() == TTC_ACK) {
                     xQueueOverwrite(tx_queue, &tx_allow);
                 }
                 else {
-                    vTaskDelayMs(2000);                         /**< wait until beacon release the antenna and send */
+                    vTaskDelayMs(2000);                         // wait until beacon release the antenna and send
                     xQueueOverwrite(tx_queue, &tx_allow);
                 }
             }
         }
 
-        /**< this task will be an aperiodic with 10s of max period */
-        if ( (last_wake_time + TTC_INTERFACE_TASK_PERIOD_TICKS) < xTaskGetTickCount() ) {
+        // this task will be an aperiodic with 10s of max period
+        if ((last_wake_time + TTC_INTERFACE_TASK_PERIOD_TICKS) < xTaskGetTickCount()) {
             last_wake_time = xTaskGetTickCount();
         }
         else {
-            vTaskDelayUntil( (TickType_t *) &last_wake_time, TTC_INTERFACE_TASK_PERIOD_TICKS );
+            vTaskDelayUntil((TickType_t *) &last_wake_time, TTC_INTERFACE_TASK_PERIOD_TICKS);
         }
     }
 
@@ -98,9 +104,9 @@ void send_shutdown() {
 
     do {
         send_command_packet(FSP_CMD_HIBERNATION);   // send the shutdown command
-        vTaskDelayMs(TIME_TO_PROCESS_CMD);          /**< wait 2 seconds until beacon process the received data  */
-        ack = receive_packet(&received_data , 0);   /**< receive the ACK/NACK                                   */
-    } while(ack != TTC_ACK);                        /**< repeat until receive ACK                               */
+        vTaskDelayMs(TIME_TO_PROCESS_CMD);          // wait 2 seconds until beacon process the received data
+        ack = receive_packet(&received_data , 0);   // receive the ACK/NACK
+    } while(ack != TTC_ACK);                        // repeat until receive ACK
 
     xSemaphoreGive(fsp_semaphore);
 }
@@ -109,14 +115,15 @@ uint8_t send_mutex_request() {
     uint8_t received_data;
     uint8_t ack;
 
-    if(xSemaphoreTake(fsp_semaphore, FSP_SEMAPHORE_WAIT_TIME) == pdPASS) {
-
-//        send_command_packet(FSP_CMD_REQUEST_RF_MUTEX);  /**< send the mutex request command                         */
-//        vTaskDelayMs(TIME_TO_PROCESS_CMD);              /**< wait 2 seconds until beacon process the received data  */
-//        ack = receive_packet(&received_data, 1);        /**< receive the ACK/NACK                                   */
+    if (xSemaphoreTake(fsp_semaphore, FSP_SEMAPHORE_WAIT_TIME) == pdPASS) {
+//        send_command_packet(FSP_CMD_REQUEST_RF_MUTEX);  // send the mutex request command
+//        vTaskDelayMs(TIME_TO_PROCESS_CMD);              // wait 2 seconds until beacon process the received data
+//        ack = receive_packet(&received_data, 1);        // receive the ACK/NACK
 
         xSemaphoreGive(fsp_semaphore);
     }
 
     return received_data;
 }
+
+//! \} End of ttc_interface_task group
